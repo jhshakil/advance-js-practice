e7c909eb103fd4f439514743d40e6efb6742fab3
Jahid Hasan <info.jhshakil@gmail.com>
2025-03-14 23:25:58 +0600

    practicing OOP

---
+++ 2 Objects/3-object-literals.js
@@ -0,0 +1,9 @@
+const groceryItem = {
+  name: "Apples",
+  quantity: 4,
+  display() {
+    console.log(`${this.quantity} x ${this.name}`);
+  },
+};
+
+groceryItem.display();
---
+++ 2 Objects/4-factory-functions.js
@@ -0,0 +1,10 @@
+let programmer = {
+  name: "Jahid",
+  preferredLanguage: "JavaScript",
+  writeCode: function () {
+    console.log(`${this.name} writes ${this.preferredLanguage} code.`);
+  },
+  drinkCoffee() {
+    console.log(`${this.name} drinks coffee`);
+  },
+};
